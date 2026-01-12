41e86a72d825bfbe2948b24102d8265ca75cc563
michaelwrobbins <81249210+michaelwrobbins@users.noreply.github.com>
2026-01-12 23:39:05 +0000

    Deploying to gh-pages from @ michaelwrobbins/michaelwrobbins.github.io@a27722eb86fc72dc44f905ef77044039a9b0ce55 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-time-series-analysis",
+          title: 'Time Series Analysis',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
