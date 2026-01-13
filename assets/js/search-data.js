51b61920a211cab4f1b8d3f620a16bb67d8b0697
michaelwrobbins <81249210+michaelwrobbins@users.noreply.github.com>
2026-01-13 18:01:11 +0000

    Deploying to gh-pages from @ michaelwrobbins/michaelwrobbins.github.io@4eac38bd34bcc071172b9a42c4c8cd2421b43885 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,9 +431,9 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-nonprobability-methods",
-          title: 'Nonprobability Methods',
-          description: "",
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-survey-panels",
