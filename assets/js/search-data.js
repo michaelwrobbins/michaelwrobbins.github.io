09b9bfbe7cd3a7434883f70704068bb3be9a1aed
michaelwrobbins <81249210+michaelwrobbins@users.noreply.github.com>
2026-01-14 00:53:08 +0000

    Deploying to gh-pages from @ michaelwrobbins/michaelwrobbins.github.io@88893fc906e6dc2d048ad7ca4427d05f6d9ee544 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-curriculum-vitae",
-          title: "Curriculum vitae",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-research",
+  },{id: "nav-research",
           title: "Research",
           description: "Research topics, software, and grants.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-curriculum-vitae",
+          title: "Curriculum Vitae",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "nav-publications",
           title: "Publications",
